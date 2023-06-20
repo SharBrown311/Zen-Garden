8d2f809ee651e06b71d6ccaf2014ac36a3ba0bcf
Sharon Brown <xSharonB030705x@gmail.com>
2023-06-20 13:08:01 -0400

    testing of master

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       <Route path = "/resume" element = {<Resume />} />
       <Route path = "/contact" element = {<Contact />} />
       </Routes>
+      <div>I Have Changed</div>
      </main>
      <Footer />
     </div>
