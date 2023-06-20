46ab76f901b63afb49dca0256b1b43bcd6023689
Sharon Brown <xSharonB030705x@gmail.com>
2023-06-20 18:16:13 -0400

    build changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions:{
-      input: {
-        app: '../client/build/index.html'
-      }
-    }
-  },
   server: {
     proxy: {
       "/api": {
