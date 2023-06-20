f8929afb34b07d73aa1978d877abd27fd1653d03
Sharon Brown <xSharonB030705x@gmail.com>
2023-06-20 18:26:03 -0400

    changes to directories

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use((err, req, res, next) => {
   return res.send({errMsg: err.message})
 })
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
