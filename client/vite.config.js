fad7bfdae38c59a2089a290fdbc0bf179e402179
Sharon Brown <xSharonB030705x@gmail.com>
2023-06-20 17:56:20 -0400

    reconfigured vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions:{
+      input: {
+        app: '../client/index.html'
+      }
+    }
+  },
   server: {
     proxy: {
       "/api": {
